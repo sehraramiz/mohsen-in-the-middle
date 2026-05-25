bf550edd0566ce1d1ce64baf0b867f7eaccc73f1
Mohsen Barzegar <sehraramiz@disroot.org>
2026-05-25 22:53:51 +0330

    refactor: improve flow highlighter performance

--- flow_highlighter.py
+++ flow_highlighter.py
@@ -1,33 +1,54 @@
 import urwid
-from mitmproxy import ctx
+from mitmproxy import ctx, http, command
+from mitmproxy.addonmanager import Loader
 from mitmproxy.tools.console import flowlist, palettes
-from mitmproxy import command
 
 
 class FlowItemWithHighlight(flowlist.FlowItem):
-    def __init__(self, master, flow):
-        self.default_color = "heading"
+    _DEFAULT_COLOR: str = "heading"
+    _focus_map: dict | None = None
+    _highlight_map: dict | None = None
+    _cache_palette: str | None = None
+    _cache_transparent: bool | None = None
+
+    def __init__(self, master: object, flow: http.HTTPFlow) -> None:
         super().__init__(master, flow)
-        urwid.WidgetWrap.__init__(self, self.get_text())
 
-    def get_text(self):
-        palette = palettes.palettes[self.master.options.console_palette].palette(
-            self.master.options.console_palette_transparent
+    def _ensure_cache(self) -> None:
+        pn = self.master.options.console_palette
+        pt = self.master.options.console_palette_transparent
+        if (
+            FlowItemWithHighlight._cache_palette != pn
+            or FlowItemWithHighlight._cache_transparent != pt
+        ):
+            palette = palettes.palettes[pn].palette(pt)
+            FlowItemWithHighlight._focus_map = {
+                item[0]: FlowItemWithHighlight._DEFAULT_COLOR for item in palette
+            }
+            FlowItemWithHighlight._highlight_map = {
+                item[0]: "focusfield_error" for item in palette
+            }
+            FlowItemWithHighlight._cache_palette = pn
+            FlowItemWithHighlight._cache_transparent = pt
+
+    def get_text(self) -> urwid.Widget:
+        self._ensure_cache()
+        if self.flow.metadata.get("highlighted", False):
+            flow_row = urwid.AttrMap(
+                super().get_text(),
+                FlowItemWithHighlight._highlight_map,
+                FlowItemWithHighlight._focus_map,
+            )
+            return urwid.AttrMap(
+                flow_row, "focusfield_error", FlowItemWithHighlight._DEFAULT_COLOR
+            )
+        flow_row = urwid.AttrMap(
+            super().get_text(), None, FlowItemWithHighlight._focus_map
         )
-        focus_map = {item[0]: self.default_color for item in palette}
-
-        highlighted = self.flow.metadata.get("highlighted", False)
-        if highlighted:
-            highlight_color = "focusfield_error"
-            highlight_map = {item[0]: highlight_color for item in palette}
-            flow_row = urwid.AttrMap(super().get_text(), highlight_map, focus_map)
-            return urwid.AttrMap(flow_row, highlight_color, self.default_color)
-        else:
-            flow_row = urwid.AttrMap(super().get_text(), None, focus_map)
-            return urwid.AttrMap(flow_row, None, self.default_color)
+        return urwid.AttrMap(flow_row, None, FlowItemWithHighlight._DEFAULT_COLOR)
 
 
-def load(loader):
+def load(loader: Loader) -> None:
     flowlist.FlowItem = FlowItemWithHighlight
 
 
